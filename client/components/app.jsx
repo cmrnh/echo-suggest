20424a2cc23726d2d656f3547c38447e5fb60696
Cameron Hendrix <cameron@burningbarnlabs.com>
2015-08-15 15:50:10 -0400

    complete milestone 5

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var http = require('axios');
 var _ = require('lodash');
 
 var Artist = require('./artist.jsx');
@@ -13,39 +14,42 @@ var App = React.createClass({
 
     handleSearchClick: function(e) {
       var artistName = React.findDOMNode(this.refs.artistName); // get the DOM node
-      artistName.value = ''; // clear the input
 
-      // TODO: Send artist name to echonest
-      // For now, we'll use mock data
-      this.setState({
-        suggestedArtists: [
-          { "id": "ARYJFJ91187B9B149A", "name": "The Wilderness" },
-          { "id": "ARGOAEZ122988F2731", "name": "Big a" },
-          { "id": "AREBMWM11F50C47FFC", "name": "Fantastic Four" },
-          { "id": "ARGMRZU1187B9B6981", "name": "The Heaters" },
-          { "id": "ARDLOZD1187B9B98A8", "name": "Boundless" },
-          { "id": "ARVXBOT1187B992D93", "name": "City to City" },
-          { "id": "ARUAQW51187B994266", "name": "Private Dancer" },
-          { "id": "ARSBVCE1187B99BE74", "name": "Hearts of Stone" },
-          { "id": "ARCF56Q1187FB55282", "name": "Airliner" }
-        ]
-      });
+      http({
+        method: 'get',
+        url: 'http://developer.echonest.com/api/v4/artist/similar',
+        headers: { 
+          'Content-Type': 'application/json' 
+        },
+        params: {
+          'name': artistName.value,
+          'api_key': 'NXYHVSJ5L3KAWFU6W'
+        }
+      }).then(function(response) {
+        this.setState({
+          suggestedArtists: response.data.response.artists
+        })
+      }.bind(this));
+
+      artistName.value = ''; // clear the input
     },
 
     handleArtistClick: function(e) {
-      this.setState({
-        suggestedArtists: [
-          { "id": "ARH1N081187B9AC562", "name": "Thom Yorke" },
-          { "id": "ARW64KS1187FB3C94D", "name": "Doves" },
-          { "id": "AR0L04E1187B9AE90C", "name": "The Verve" },
-          { "id": "ARTNON61187B98D6EE", "name": "Elbow" },
-          { "id": "ARG7LMD1187FB4B064", "name": "Mansun" },
-          { "id": "ARKVITV1187B9AE854", "name": "Blur" },
-          { "id": "ARZ0RS81187B98F252", "name": "Mercury Rev" },
-          { "id": "ARIIMPS1187FB4CD03", "name": "Richard Ashcroft" },
-          { "id": "ARZNOIY1187B989D9C", "name": "On a Friday" }
-        ]
-      });
+      http({
+        method: 'get',
+        url: 'http://developer.echonest.com/api/v4/artist/similar',
+        headers: { 
+          'Content-Type': 'application/json' 
+        },
+        params: {
+          'name': e.target.text,
+          'api_key': 'NXYHVSJ5L3KAWFU6W'
+        }
+      }).then(function(response) {
+        this.setState({
+          suggestedArtists: response.data.response.artists
+        })
+      }.bind(this));
     },
 
     render: function() {
