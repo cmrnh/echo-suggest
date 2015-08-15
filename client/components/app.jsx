75fabb471983c0abb269994020f267c14002d8a6
Cameron Hendrix <cameron@burningbarnlabs.com>
2015-08-15 14:51:13 -0400

    complete milestone 2

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -2,9 +2,18 @@ var React = require('react');
 
 var App = React.createClass({
 
+    handleSearchClick: function() {
+      var artistName = React.findDOMNode(this.refs.artistName); // get the DOM node
+      artistName.value = ''; // clear the input
+    },
+
     render: function(){
       return (
-        <div>Hello, World!</div>
+        <div>
+          <h1>EchoSuggest</h1>
+          <input placeholder='artist name' />
+          <button onClick={ this.handleSearchClick }>Search</button>
+        </div>
       );
     }
 
